d4378b1115318abd7cb5efb80b59b3b5eb954bbd
Carl Klein <carlklein@MacBookPro-CKC.fritz.box>
2018-02-25 23:34:53 +0100

    Create Test skill

--- __init__.py
+++ __init__.py
@@ -15,11 +15,11 @@ from mycroft.util.log import LOG
 # from the MycroftSkill class.  You extend this class as shown below.
 
 # TODO: Change "Template" to a unique name for your skill
-class TemplateSkill(MycroftSkill):
+class TestSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
-        super(TemplateSkill, self).__init__(name="TemplateSkill")
+        super(TestSkill, self).__init__(name="TestSkill")
         
         # Initialize working variables used within the skill.
         self.count = 0
@@ -62,4 +62,4 @@ class TemplateSkill(MycroftSkill):
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
 def create_skill():
-    return TemplateSkill()
+    return TestSkill()
